3c2a79187d1aa9425eb50e877699f42717b701c4
Mohammed alshaheri <malshaheri@gmail.com>
2024-10-27 02:51:54 +0200

    adding some changes and opening times

--- main.js
+++ main.js
@@ -8,3 +8,44 @@ menu.onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+const time = () => {
+
+    const activerow = document.querySelector('#activerow');
+
+    const monday = document.querySelector('.monday');
+    const tuesday = document.querySelector('.tuesday');
+    const wednesday = document.querySelector('.wednesday');
+    const thursday = document.querySelector('.thursday');
+    const friday = document.querySelector('.friday');
+    const saturday = document.querySelector('.saturday');
+    const sunday = document.querySelector('.sunday');
+
+
+    switch (new Date().getDay()) {
+
+        case 1:
+            monday.setAttribute("id", "activerow");
+            break;
+        case 2:
+            tuesday.setAttribute("id", "activerow");
+            break;
+        case 3:
+            wednesday.setAttribute("id", "activerow");
+            break;
+        case 4:
+            thursday.setAttribute("id", "activerow");
+            break;
+        case 5:
+            friday.setAttribute("id", "activerow");
+            break;
+        case 6:
+            saturday.setAttribute("id", "activerow");
+            break;
+        case 0:
+            sunday.setAttribute("id", "activerow");
+            break;
+    }
+
+}
+time();
